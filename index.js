44797339e1c2151dce4ed1cc498762d84ae9208e
EFREN-GTH <efrenojeda9@gmail.com>
2022-12-08 15:44:35 +0100

    Terminado (siguiendo un tutorial)

---
+++ index.js
@@ -0,0 +1,14 @@
+const pantallaValorAnterior = document.getElementById('valor-anterior');
+const pantallaValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const pantalla = new Pantalla(pantallaValorAnterior, pantallaValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => pantalla.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => pantalla.computar(boton.value))
+})
